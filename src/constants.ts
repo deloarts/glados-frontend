493101f541c7027cb9acbbf41e71aa1b1b13edd0
Philip Delorenzo <git@deloarts.com>
2023-02-27 17:18:46 +0100

    Update fetch interval

--- src/constants.ts
+++ src/constants.ts
@@ -6,11 +6,11 @@ export default {
   minWidth: 1450,
   
   // Watch intervals
-  watchServerConnInterval: 10000,
+  watchServerConnInterval: 4000,
 
   // Fetch intervals
   fetchUsersAfter: 60000,
-  fetchBoughtItemsAfter: 60000,
+  fetchBoughtItemsAfter: 5000,
 
   // API definition
   apiAccessToken: "/api/web/v1/login/access-token",

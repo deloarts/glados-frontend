3c7d4d8741297171c15d9482068218334c68eeaa
Philip Delorenzo <git@deloarts.com>
2024-08-23 08:21:04 +0200

    Fix route to 1d solver

--- src/requests/tools.ts
+++ src/requests/tools.ts
@@ -4,7 +4,7 @@ export class ToolsStockCutRequest {
   // POST
   postSolver(data: object) {
     return request.post(
-      "/api/web/v1/tools/stock-cut/solve",
+      "/api/web/v1/tools/stock-cut/1d/solve",
       requestConfig(null),
       data,
     );

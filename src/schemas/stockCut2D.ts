e9f567235f478654e97f4355f644fa21f4f1c55b
Philip Delorenzo <git@deloarts.com>
2025-01-10 10:01:42 +0100

    downgrade StockCut2D in backend and update schema

--- src/schemas/stockCut2D.ts
+++ src/schemas/stockCut2D.ts
@@ -1,47 +1,47 @@
 export interface StockCut2DItem {
-  id: string;
-  width: number;
-  height: number;
-  can_rotate: boolean;
+  id: string
+  width: number
+  height: number
+  can_rotate: boolean
 }
 
 export interface StockCut2DPanel {
-  id: string;
-  width: number;
-  height: number;
+  id: string
+  width: number
+  height: number
 }
 
 export interface StockCut2DParams {
-  items: Array<StockCut2DItem>;
-  panels: Array<StockCut2DPanel>;
-  cut_width: number;
-  min_initial_usage: boolean;
+  items: Array<StockCut2DItem>
+  panels: Array<StockCut2DPanel>
+  cut_width: number
+  min_initial_usage: boolean
 }
 
 export interface StockCut2DUsed {
-  panel: StockCut2DPanel;
-  item: StockCut2DItem;
-  x: number;
-  y: number;
-  rotate: boolean;
+  panel: StockCut2DPanel
+  item: StockCut2DItem
+  x: number
+  y: number
+  rotate: boolean
 }
 
 export interface StockCut2DUnused {
-  panel: StockCut2DPanel;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
+  panel: StockCut2DPanel
+  x: number
+  y: number
+  width: number
+  height: number
 }
 
 export interface StockCut2DJobSchema {
-  params: StockCut2DParams;
-  method: string;
+  params: StockCut2DParams
+  method: string
 }
 
 export interface StockCut2DResultSchema {
-  params: StockCut2DParams;
-  used: Array<StockCut2DUsed>;
-  unused: Array<StockCut2DUnused>;
-  cuts: Array<string>;
+  params: StockCut2DParams
+  used: Array<StockCut2DUsed>
+  unused: Array<StockCut2DUnused>
+  // cuts: Array<string>;
 }

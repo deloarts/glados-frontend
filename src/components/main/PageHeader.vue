a379b58d0916b0d831935c12b13f21f765a42fa6
Philip Delorenzo <git@deloarts.com>
2025-02-27 08:49:18 +0100

    Update header and footer

--- src/components/main/PageHeader.vue
+++ src/components/main/PageHeader.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import config from '@/config'
 
 import QuickTheme from '@/components/common/QuickTheme.vue'
 import QuickLanguage from '@/components/common/QuickLanguage.vue'
